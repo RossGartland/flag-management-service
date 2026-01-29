9fda37912531db8798790e035bbe80f0c8ad78b8
RossGartland <rgartland117@hotmail.com>
2026-01-29 21:38:25 +0000

    Initial spring boot setup

---
+++ src/main/java/com/rg/flag_management_service/FlagManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rg.flag_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rg/flag_management_service/FlagManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rg.flag_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
